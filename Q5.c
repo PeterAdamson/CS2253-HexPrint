6738f02f9e0f6a054181ae70049c4e76e27c8c84
PeterAdamson <adamsonpeterj@gmail.com>
2018-01-16 17:59:40 -0400

    Create Q5.c

---
+++ Q5.c
@@ -0,0 +1,15 @@
+//Author Peter Adamson 3319005
+
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+	int a, b, c[3], d;
+	printf("%lu\n",sizeof(int));
+	printf("%X\n",&a);
+	printf("%X\n",&a - 1);
+	printf("%X\n",&b);
+	printf("%X\n",&c);
+	printf("%X\n",&d);
+	return 0;
+}
